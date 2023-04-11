345e50f85e0bccff8ef2b6fe82cfeb6cf15c3b35
CMLTTN <cemaldim@gmail.com>
2023-04-11 14:19:38 +0300

    clg

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-function writeWords(ab,s){
+// function writeWords(ab,s){
   
-     for(let i = 1; i<= s;i++){
+//      for(let i = 1; i<= s;i++){
      
-          console.log(ab);
-          console.log(" ");
+//           console.log(ab);
+//           console.log(" ");
 
-     }
+//      }
      
 
+// }
+
+// writeWords("hello", 5);
+// writeWords("ali", 5);
+
+//? dikdörtgenib alan ve çevresini hesaplayan 
+
+function alanCevreHesapla(kisa,uzun){
+
+     let alan = kisa*uzun;
+     let cevre=(kisa+uzun)*2;
+     return `alan : ${alan} cevre: ${cevre}`;
 }
-
-writeWords("hello", 5);
-writeWords("ali", 5);
+let sonuc = alanCevreHesapla(3,9)
+console.log(alanCevreHesapla(3, 33));
+console.log(alanCevreHesapla(4, 2));
+console.log(alanCevreHesapla(6, 5));
+console.log(alanCevreHesapla(2, 9));
