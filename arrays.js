16bb16d71ffce4a7f1ca41ba49ed0c43b10c38aa
CMLTTN <cemaldim@gmail.com>
2023-04-12 13:16:46 +0300

    change

--- arrays.js
+++ arrays.js
@@ -1,3 +1,32 @@
-const names = ["ali", "veli"]
+const names = ["ali", "veli", "ayşe", "musa"]
 
+names[2]= "selahaddin"
 console.log(names);
+// console.log(names[2]);
+// console.log(names);
+// console.log(typeof names);
+// const emptyAr = []
+
+// names[0].toUpperCase()
+
+// console.log(names[0].toUpperCase());
+// console.log(names);
+// // const a = names.toUpperCase()
+
+// let arr = ['apple', 'banana', 'orange'];
+// let uppercaseArr = arr.map(function(item) {
+//   return item.toUpperCase();
+// });
+// console.log(uppercaseArr); 
+// console.log(arr[2]);//
+
+// output: ['APPLE', 'BANANA', 'ORANGE']
+
+
+// console.log(arr[1]);
+// // const isimler = ["ali", "veli"];
+// const axel = isimler.map((f) => {
+//   return f.toUpperCase();
+// });
+// // console.log(isimler[0].toUpperCase());
+// console.log(axel);
